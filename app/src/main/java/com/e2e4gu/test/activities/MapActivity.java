2c094d2e7232acf1e1f6215fdc05eb650362c652
DizelBadCoder <diskroma69@gmail.com>
2021-01-21 18:21:01 +0700

    add: runtime location

--- app/src/main/java/com/e2e4gu/test/activities/MapActivity.java
+++ app/src/main/java/com/e2e4gu/test/activities/MapActivity.java
@@ -1,21 +1,48 @@
 package com.e2e4gu.test.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.location.Location;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.e2e4gu.test.R;
 
+import com.mapbox.android.core.permissions.PermissionsListener;
+import com.mapbox.android.core.permissions.PermissionsManager;
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.location.LocationComponent;
+import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions;
+import com.mapbox.mapboxsdk.location.LocationComponentOptions;
+import com.mapbox.mapboxsdk.location.modes.CameraMode;
+import com.mapbox.mapboxsdk.location.modes.RenderMode;
 import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.Style;
 
-public class MapActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MapActivity
+        extends AppCompatActivity
+        implements OnMapReadyCallback, PermissionsListener {
     private final String STYLE_URI = "mapbox://styles/dizelbadcoder/ckk5g8f991k4g17qqdj6bls5q";
+
+    private TextView textViewDebug;
+    private Location currentLocation;
+    private PermissionsManager permissionsManager;
+    private Style fullyLoadedStyleMap;
     private MapView mapView;
+    private MapboxMap mapboxMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,11 +50,20 @@ public class MapActivity extends AppCompatActivity {
                 getResources().getString(R.string.mapbox_access_token));
         setContentView(R.layout.activity_map);
         mapView = findViewById(R.id.mapView);
+        textViewDebug = findViewById(R.id.textViewDebug);
         mapView.onCreate(savedInstanceState);
-        mapView.getMapAsync(mapboxMap -> mapboxMap.setStyle(
-                new Style.Builder().fromUri(STYLE_URI), style -> {
+        mapView.getMapAsync(this);
+    }
 
-        }));
+    @Override
+    public void onMapReady(@NonNull MapboxMap mapboxMap) {
+        this.mapboxMap = mapboxMap;
+        mapboxMap.setStyle(new Style.Builder().fromUri(STYLE_URI), new Style.OnStyleLoaded() {
+            @Override
+            public void onStyleLoaded(@NonNull Style style) {
+                fullyLoadedStyleMap = style;
+            }
+        });
     }
 
     public void addMarker(View view) {
@@ -40,12 +76,59 @@ public class MapActivity extends AppCompatActivity {
             .setTitle(R.string.dialog_position_title)
             .setMessage(R.string.dialog_position_label)
             .setPositiveButton(R.string.dialog_position_life, (dialog, which) -> {
-
+                enableLocationComponent();
             })
             .setNegativeButton(R.string.dialog_position_test, (dialog, which) -> {
 
             })
             .show();
+    }
+
+    @SuppressWarnings({"MissingPermission"})
+    private void enableLocationComponent() {
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+            LocationComponentOptions customLocationComponentOptions =
+                    LocationComponentOptions.builder(this)
+                    .pulseEnabled(false)
+                    .build();
+
+            LocationComponent locationComponent = mapboxMap.getLocationComponent();
+            locationComponent.activateLocationComponent(
+                    LocationComponentActivationOptions
+                            .builder(this, fullyLoadedStyleMap)
+                            .locationComponentOptions(customLocationComponentOptions)
+                            .build());
+            locationComponent.setLocationComponentEnabled(true);
+        } else {
+            permissionsManager = new PermissionsManager(this);
+            permissionsManager.requestLocationPermissions(this);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public void onExplanationNeeded(List<String> permissionsToExplain) {
+
+    }
+
+    @Override
+    public void onPermissionResult(boolean granted) {
+        if (granted) {
+            mapboxMap.getStyle(new Style.OnStyleLoaded() {
+                @Override
+                public void onStyleLoaded(@NonNull Style style) {
+                    enableLocationComponent();
+                }
+            });
+        } else {
+            Toast.makeText(this, "Not granted", Toast.LENGTH_LONG).show();
+            finish();
+        }
     }
 
     @Override
