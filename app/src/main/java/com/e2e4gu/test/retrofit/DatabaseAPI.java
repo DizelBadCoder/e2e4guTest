2891b3bccf7cbd948734e01ee4db220a53623a2d
DizelBadCoder <diskroma69@gmail.com>
2021-01-21 18:19:44 +0700

    fix: retrofit callback

--- app/src/main/java/com/e2e4gu/test/retrofit/DatabaseAPI.java
+++ app/src/main/java/com/e2e4gu/test/retrofit/DatabaseAPI.java
@@ -3,10 +3,11 @@ package com.e2e4gu.test.retrofit;
 import com.e2e4gu.test.retrofit.models.Marker;
 import java.util.List;
 
+import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface DatabaseAPI {
     @GET("markers")
-    List<Marker> getMarkerList(@Query("x") float x, @Query("y") float y, @Query("radius") float radius);
+    Call<List<Marker>> getMarkerList(@Query("x") float x, @Query("y") float y);
 }
