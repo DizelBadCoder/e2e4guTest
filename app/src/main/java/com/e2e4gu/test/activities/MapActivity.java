75a2384dbf7f502945466afa7bf10e927bcad6b2
DizelBadCoder <diskroma69@gmail.com>
2021-01-21 20:39:35 +0700

    add: movement camera to current position

--- app/src/main/java/com/e2e4gu/test/activities/MapActivity.java
+++ app/src/main/java/com/e2e4gu/test/activities/MapActivity.java
@@ -1,36 +1,40 @@
 package com.e2e4gu.test.activities;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Camera;
 import android.location.Location;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.os.Message;
-import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.e2e4gu.test.R;
 
+import com.e2e4gu.test.retrofit.DatabaseAPI;
+import com.e2e4gu.test.retrofit.RetrofitUtils;
+import com.e2e4gu.test.retrofit.models.Marker;
 import com.mapbox.android.core.permissions.PermissionsListener;
 import com.mapbox.android.core.permissions.PermissionsManager;
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.camera.CameraUpdate;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
+import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.location.LocationComponent;
 import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions;
 import com.mapbox.mapboxsdk.location.LocationComponentOptions;
-import com.mapbox.mapboxsdk.location.modes.CameraMode;
-import com.mapbox.mapboxsdk.location.modes.RenderMode;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.Style;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MapActivity
         extends AppCompatActivity
@@ -38,11 +42,11 @@ public class MapActivity
     private final String STYLE_URI = "mapbox://styles/dizelbadcoder/ckk5g8f991k4g17qqdj6bls5q";
 
     private TextView textViewDebug;
-    private Location currentLocation;
     private PermissionsManager permissionsManager;
     private Style fullyLoadedStyleMap;
     private MapView mapView;
     private MapboxMap mapboxMap;
+    private Location currentLocation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,6 +57,7 @@ public class MapActivity
         textViewDebug = findViewById(R.id.textViewDebug);
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(this);
+        //TODO release textview debug
     }
 
     @Override
@@ -62,17 +67,19 @@ public class MapActivity
             @Override
             public void onStyleLoaded(@NonNull Style style) {
                 fullyLoadedStyleMap = style;
+                checkMyLocation(null);
             }
         });
     }
 
     public void addMarker(View view) {
-        //TODO
+        //TODO 
         Toast.makeText(this, "Developing...", Toast.LENGTH_LONG).show();
     }
 
     public void checkMyLocation(View view) {
-        enableLocationComponent();
+        if (fullyLoadedStyleMap != null)
+            enableLocationComponent();
     }
 
     @SuppressWarnings({"MissingPermission"})
@@ -90,6 +97,8 @@ public class MapActivity
                             .locationComponentOptions(customLocationComponentOptions)
                             .build());
             locationComponent.setLocationComponentEnabled(true);
+            currentLocation = locationComponent.getLastKnownLocation();
+            moveCameraToMyLocation();
         } else {
             permissionsManager = new PermissionsManager(this);
             permissionsManager.requestLocationPermissions(this);
@@ -118,6 +127,15 @@ public class MapActivity
             });
         } else {
             Toast.makeText(this, "Not granted", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void moveCameraToMyLocation() {
+        if (currentLocation != null) {
+            mapboxMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                    new LatLng(currentLocation.getLatitude(),
+                            currentLocation.getLongitude(),
+                            currentLocation.getAltitude()), 15), 1200);
         }
     }
 
